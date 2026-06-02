0c3cdda1a5e65b68cccb1834e199c4ed26c269b8
danramirez11 <daramirezp1112@gmail.com>
2026-06-02 11:01:27 -0500

    generate base

--- Skilltesting/src/App.tsx
+++ Skilltesting/src/App.tsx
@@ -1,16 +1,14 @@
 ﻿import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import ComponentsShowcase from "./components/ComponentsShowcase";
-import WorkspaceCreationScreen from "./screens/WorkspaceCreationScreen";
 import "./styles/Layout.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<WorkspaceCreationScreen />} />
         <Route
-          path="/components"
+          path="/"
           element={
             <div className="app-layout">
               <Navigation />
